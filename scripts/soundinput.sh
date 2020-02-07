50509a4dddfcbad355e29e228fa58610c9b7363a
SpringHan <3379184879@qq.com>
2020-02-07 17:38:54 +0800

    Changed a option

--- scripts/soundinput.sh
+++ scripts/soundinput.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sound=$(zenity --title Sound --text Input sound --width 500 --entry 2>/dev/null)
-eval "pactl -- set-sink-volume 0 ${sound}%"
+if [[ -n "${sound}" ]];then
+	eval "pactl -- set-sink-volume 0 ${sound}%"
+fi
