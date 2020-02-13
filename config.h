91580e679d9c7f67fc6f3a2ea814293bab0eb84a
SpringHan <3379184879@qq.com>
2020-02-13 17:28:44 +0800

    Added a new key map

--- config.h
+++ config.h
@@ -72,6 +72,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *rangercmd[] = { "st ranger", NULL };
 static const char *browsercmd[] = { "google-chrome-stable", NULL };
 static const char *musicmd[] = { "netease-cloud-music", NULL };
 static const char *imagecmd[] = { "gimp", NULL };
@@ -97,6 +98,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = rangercmd } },
 	{ MODKEY,                       XK_g,      spawn,          {.v = browsercmd } },
 	{ MODKEY|ShiftMask,             XK_m,      spawn,          {.v = musicmd } },
 	{ MODKEY|ShiftMask,             XK_g,      spawn,          {.v = imagecmd } },
