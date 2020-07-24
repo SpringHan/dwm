c2ca177ad5413bf8bc203ddd2a0184967fd58152
SpringHan <3379184879@qq.com>
2020-07-24 14:31:20 +0800

    Now use fcitx5 instead of fcitx4

--- scripts/wait-start.sh
+++ scripts/wait-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sleep 5
-[ "$(ps ax|grep -v grep|grep fcitx)" == "" ] && fcitx &
+[ "$(ps ax|grep -v grep|grep fcitx)" == "" ] && fcitx5 &
 sleep 5
 [ "$(ps ax|grep -v grep|grep kdeconnect-indicator)" == "" ] && [ "$(ps ax|grep -v grep|grep fcitx)" != "" ] && kdeconnect-indicator &
