82d5ade0b8fe59f8063883b606e8a38a6e216b45
SpringHan <3379184879@qq.com>
2020-08-19 18:40:08 +0800

    Modified the picture script

--- scripts/picture.sh
+++ scripts/picture.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-time=$(date "+%H")
-
-if [ ${time} -ge 18 ] || [ ${time} -lt 6 ]
-then
-	feh --randomize --bg-fill ~/picture_dark
-else
-	feh --randomize --bg-fill ~/picture
-fi
+feh --randomize --bg-fill ~/picture
