e7a67a7881b109532a407458da4061cb67fbd2b3
SpringHan <3379184879@qq.com>
2020-10-16 16:22:14 +0800

    Added xrog's set

--- scripts/wait-start.sh
+++ scripts/wait-start.sh
@@ -4,3 +4,6 @@ sleep 5
 [ "$(ps ax|grep -v grep|grep fcitx)" == "" ] && fcitx5 &
 sleep 5
 [ "$(ps ax|grep -v grep|grep kdeconnect-indicator)" == "" ] && [ "$(ps ax|grep -v grep|grep fcitx)" != "" ] && kdeconnect-indicator &
+sleep 5
+xset s 0
+xset dpms 0
