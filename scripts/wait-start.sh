41d979075a25f6c959ffd8c2f96aa4b34b0df4e4
SpringHan <3379184879@qq.com>
2020-10-17 21:15:48 +0800

    Modified the script

--- scripts/wait-start.sh
+++ scripts/wait-start.sh
@@ -6,4 +6,4 @@ sleep 5
 [ "$(ps ax|grep -v grep|grep kdeconnect-indicator)" == "" ] && [ "$(ps ax|grep -v grep|grep fcitx)" != "" ] && kdeconnect-indicator &
 sleep 5
 xset s 0
-xset dpms 0
+xset dpms 0 0 0
