d5c480ae437e3698dd29c2af43a48d18b031286e
SpringHan <3379184879@qq.com>
2020-11-28 12:57:26 +0800

    modifiy(wait-start.sh): Commented fcitx5's start

--- scripts/wait-start.sh
+++ scripts/wait-start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# sleep 5
+# [ "$(ps ax|grep -v grep|grep fcitx)" == "" ] && fcitx5 &
 sleep 5
-[ "$(ps ax|grep -v grep|grep fcitx)" == "" ] && fcitx5 &
-sleep 5
-[ "$(ps ax|grep -v grep|grep kdeconnect-indicator)" == "" ] && [ "$(ps ax|grep -v grep|grep fcitx)" != "" ] && kdeconnect-indicator &
+[ "$(ps ax|grep -v grep|grep kdeconnect-indicator)" == "" ] && kdeconnect-indicator &
 sleep 5
 xset s 0
 xset dpms 0 0 0
