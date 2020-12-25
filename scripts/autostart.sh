ad6005f686f0a6057bc561d2ac7b56eeef59feab
SpringHan <3379184879@qq.com>
2020-12-25 20:39:15 +0800

    modifiy(autostart.sh): Deleted a async script

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,5 +2,4 @@
 
 /bin/bash ~/Github/dwm/scripts/dwm-status-refresh.sh &
 /bin/bash ~/Github/dwm/scripts/wait-start.sh &
-/bin/bash ~/Github/dwm/scripts/picture-refresh.sh &
 /bin/bash ~/Github/dwm/scripts/picomstart.sh &
