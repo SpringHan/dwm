1a374b38b544c6dd0f1dad4f321181b81d53c4ac
SpringHan <3379184879@qq.com>
2022-12-25 14:04:42 +0800

    Removed an autoload program

--- scripts/wait-start.sh
+++ scripts/wait-start.sh
@@ -2,8 +2,8 @@
 
 # sleep 5
 # [ "$(ps ax|grep -v grep|grep fcitx)" == "" ] && fcitx5 &
-sleep 5
-[ "$(ps ax|grep -v grep|grep kdeconnect-indicator)" == "" ] && kdeconnect-indicator &
+# sleep 5
+# [ "$(ps ax|grep -v grep|grep kdeconnect-indicator)" == "" ] && kdeconnect-indicator &
 sleep 5
 
 xset s 0
