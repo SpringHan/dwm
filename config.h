1d2b06f49521d6a66717036b7a61c0d8b9397f66
springhan <3379184879@qq.com>
2023-10-17 16:26:56 +0800

    Changed font

--- config.h
+++ config.h
@@ -15,8 +15,8 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Source Code Pro:size=15" };
-static const char dmenufont[]       = "Source Code Pro:size=10";
+static const char *fonts[]          = { "Monego:size=15" };
+static const char dmenufont[]       = "Monego:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
